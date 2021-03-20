33d38ef1671f4f44684e596f4487df3a115c53d1
Tomasz Klara <tomaszklara@gmail.com>
2021-03-20 09:58:51 +0000

    added home and albums templates

--- main.go
+++ main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 
+const portNumber = ":8080"
+
+// Home is home page handler
+func Home(w http.ResponseWriter, r *http.Request)  {
+	renderTemplate(w,"home.page.tmpl")
+}
+
+// Albums page handler
+func Albums(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "albums.page.tmpl")
+}
+
+// renderTemplate render templates handler
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	// Parse template
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, "Hello World")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/albums", Albums)
 
-		if err != nil {
-			fmt.Printf("Error is %s", err)
-		}
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
